a692f60f4fc21960d3763cc73207b207b65de65b
Jacob Menick <jmenick@reed.edu>
2014-07-10 17:35:39 -0700

    updated reuters_analysis demo

--- reuters_analysis.R
+++ reuters_analysis.R
@@ -1,32 +1,40 @@
-## This file runs the analysis on the reuters001 corpus.  
-## It assumes our current directory is the root of this project.  
+# Run the analysis on a reuters corpus. 
 
-# Load functions into workspace
+require(Rcpp)
+
+# Load the functions for the package into the namespace.
 source("gibbs_prep.R")
-source("Gibbs.R")
+sourceCpp("devGibbs.cpp")
 source("gibbs_output.R")
 
-# Store the path to the reuters001 corpus as a string. 
-path_to_reuters001 <- "text_corpuses/reuters001"
+# Get the reuters corpus from the text files. 
+path_to_reuters <- "text_corpuses/reuters001"
+reuters001 <- get_corpus(path_to_reuters)
+dtm001 <- get_dtm(reuters001)
 
-# Get corpus and dtm.
-reuters001 <- get_corpus(path_to_reuters001)
-dtm001 <- get_dtm_matrix(reuters001)
-
-# Get vocabulary object. 
-vocab001 <- get_vocabObj(reuters001)
-
-# Set Parameters for the Gibbs Sampler
-n.sim <- 25
+# Set model parameters. 
 K <- 10
 alpha <- 50/K
 beta <- 0.01
+nsim <- 50
 
-# Run Gibbs Sampler and store results in the variable 'params001'
-params001 <- gibbs.sampler.lda(dtm001, n.sim, K, alpha, beta)
+# Run the model to get parameter estimate for phi and theta. 
+params <- gibbsC(dtm001, 1, K, alpha, beta, verbose = F)
 
-# Let's visualize the results by making plots. 
-df <- get_plottable_df(reuters001, dtm001, vocab001, params001, 10)
-
+# Plot the results with ggplot2.
+df <- get_plottable_df_lda(dtm001, colnames(dtm001), params, 10)
 plot <- get_primitive_qplot(df, 10)
 print(plot)
+
+
+
+
+
+
+
+
+
+
+
+
+
