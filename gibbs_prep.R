b5592480003bcaa52ae7f037c954ba4051bc9f01
Jacob Menick <jmenick@reed.edu>
2014-08-08 12:55:13 -0700

    brought gibbs_prep funcs up to latest tm package version

--- gibbs_prep.R
+++ gibbs_prep.R
@@ -11,7 +11,7 @@ get_corpus <- function(path) {
   corpus <- tm_map(corpus, removeNumbers)
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, stripWhitespace)
-  corpus <- tm_map(corpus, tolower)
+	corpus <- tm_map(corpus, content_transformer(tolower))
   corpus <- tm_map(corpus, removeWords, stopwords("english"))
 #  corpus <- tm_map(corpus, stemDocument, language="english")
   corpus
