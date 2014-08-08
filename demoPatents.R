60ee3e9d0669982e0617b810596b6a7badce4603
Jacob Menick <jmenick@reed.edu>
2014-08-08 13:13:58 -0700

    brought patent demo up to date

--- demoPatents.R
+++ demoPatents.R
@@ -2,21 +2,21 @@
 ## obtained from the USPTO. 
 ## Thanks to Drew Blount for his XMLParser.  
 ## 
-## The program assumes our current directory is the root of this project.  
-## Note - this is a pretty big corpus, so the algo takes awhile to run:
-##      - around 10 minutes with 25 iterations.
+
+require(Rcpp)
 
 # Load functions into workspace
 source("gibbs_prep.R")
-source("Gibbs.R")
 source("gibbs_output.R")
+sourceCpp("gibbs.cpp")
 
 # Store the path to the reuters001 corpus as a string. 
 path_to_patents <- "/Users/jacobmenick/Desktop/Summer_2014_Research/r_scripts/LdaGibbs/text_corpuses/patents_jan07"
 
 # Get corpus and dtm.
 corpusPATENTS <- get_corpus(path_to_patents)
-dtmPATENTS <- get_dtm_matrix(corpusPATENTS)
+corpusPATENTS <- sample(corpusPATENTS, 1000)
+dtmPATENTS <- get_dtm(corpusPATENTS)
 vocabPATENTS <- colnames(dtmPATENTS)
 
 # Remove stopwords.
@@ -27,11 +27,13 @@ dtmPATENTS <- new_objs[[1]]
 vocabPATENTS <- new_objs[[2]]
 
 # Set Parameters for the LDA Model
+nsim <- 25
 K <- 30
 alpha <- 50/K
+beta <- 0.01
 
 # Run the LDA code from the 'topicmodels' package. 
-paramsPATENTS <- get_params_from_topicmodels(dtmPATENTS, K)
+paramsPATENTS <- gibbsC(dtmPATENTS, nsim, K, alpha, beta, verbose=F)
 
 # Let's visualize the results by making plots. 
 df <- get_plottable_df(corpusPATENTS, dtmPATENTS, colnames(dtmPATENTS), paramsPATENTS, 10)
